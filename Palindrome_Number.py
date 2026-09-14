f6240af48a883b09757d713b230c16a3877c3488
mahendra shetty <mahendrashetty9632@gmail.com>
2026-09-14 11:09:44 +0530

    Day-02

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -5,7 +5,8 @@ class Solution(object):
         original = x
         reversed_x = 0
         while x > 0:
-            reversed_x = reversed_x * 10 + x % 10
+            num= x % 10
+            reversed_x = reversed_x * 10 + num
             x //= 10
         return reversed_x == original
 
