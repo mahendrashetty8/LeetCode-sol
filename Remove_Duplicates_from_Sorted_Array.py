2c032b6d5ce39cee4e34e3fc452476c9fea115df
mahendra shetty <mahendrashetty9632@gmail.com>
2026-09-15 21:21:44 +0530

    Day to day commit

--- Remove_Duplicates_from_Sorted_Array.py
+++ Remove_Duplicates_from_Sorted_Array.py
@@ -7,3 +7,6 @@ class Solution(object):
         for i in range(len(fre)):
             nums[i]=fre[i]
         return len(fre)
+sol=Solution()
+nums = [0,0,1,1,1,2,2,3,3,4]
+sol.removeDuplicates(nums)
